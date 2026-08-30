60580e816050b926814f4e956f5167d9fa3d7fd4
Zisanur Rahman <zisanurrahmanbd@gmail.com>
2026-08-30 13:46:17 +0600

    Fix HTTPS reverse proxy trust and force HTTPS for Render cloud deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
